c71729551bfe1e333ce61f8be131a4d76385c12d
Spielfuchx <87388119+Spielfuchx@users.noreply.github.com>
2025-06-21 13:42:53 +0200

    txadmin_zwei.sh

--- txadmin_zwei.sh
+++ txadmin_zwei.sh
@@ -1,48 +1,44 @@
-#!/bin/bash
-
 echo 'FiveM TxAdmin Linux Installer von SpielFuchx'
 
-# Notwendige Pakete installieren
-apt update
-apt install -y xf tar wget screen cron
+apt install xf tar
 
-# Verzeichnisstruktur erstellen
 mkdir -p /home/FiveM/server
 cd /home/FiveM/server
 
-# Benutzer nach dem Artifact-Link fragen
 echo 'Geben Sie den Link zu den FiveM-Artifakten ein:'
 read link
-wget $link -O fx.tar.xz
+wget $link
 
-# Artefakte entpacken
 echo 'Entpacken der FiveM-Dateien...'
 tar xf fx.tar.xz
-rm -r fx.tar.xz
 echo 'Artifacts installiert'
 
-# Start- und Stop-Skripte erstellen
+rm -r fx.tar.xz
+
+echo 'Installieren von Screen...'
+apt install screen
+
+echo ' crontab wird installiert und eingerichtet'
+
+(crontab -l 2>/dev/null; echo "@reboot /bin/bash /home/FiveM/server/run.sh > /home/FiveM/server/cron.log 2>&1") | crontab -
+
+cd /home/FiveM/server && screen ./run.sh
+
+echo 'Erfolgreich installiert! Jetzt müssen Sie in den Ordner cd /home/FiveM/server wechseln und die Datei run.sh ausführen --> ./run.sh'
+
+echo 'Außerdem wurde ein Crontab erfolgreich installiert. Bei einen Server neustart wird FIveM / TxAdmin automatisch Gestartet.'
+
+# START.SH HINZUFÜGEN
 cat << 'EOF' > /home/FiveM/server/start.sh
 #!/bin/bash
 cd /home/FiveM/server
 screen -dmS fivem ./run.sh
 EOF
 
+# STOP.SH HINZUFÜGEN
 cat << 'EOF' > /home/FiveM/server/stop.sh
 #!/bin/bash
 screen -S fivem -X quit
 EOF
 
 chmod +x /home/FiveM/server/start.sh /home/FiveM/server/stop.sh
-
-# Crontab für Autostart einrichten
-echo 'Crontab wird installiert und eingerichtet'
-(crontab -l 2>/dev/null; echo "@reboot /home/FiveM/server/start.sh > /home/FiveM/server/cron.log 2>&1") | crontab -
-
-# Server direkt starten
-/home/FiveM/server/start.sh
-
-echo 'Erfolgreich installiert!'
-echo 'Jetzt können Sie den Server starten mit: ./start.sh'
-echo 'Oder stoppen mit: ./stop.sh'
-echo 'Die Datei run.sh wird beim Systemstart automatisch ausgeführt.'
